36236589ee7b3f5a5d67b5cc97dae045dcbca236
Vagner Wentz <wentz.vagner@gmail.com>
2022-06-25 21:52:40 -0300

    feat: Add the create, list, edit and delete a Category.

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -22,4 +22,92 @@ public class CategoryController : Controller
 
         return View(categoryList);
     }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+            return NotFound();
+
+        var category = _db.Categories.Find(id);
+        // var categoryFromFirst = _db.Categories.FirstOrDefault(category => category.Id == id);
+        // var categoryFromSingle = _db.Categories.SingleOrDefault(category => category.Id == id);
+
+        if (category == null)
+            return NotFound();
+        
+        return View(category);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category category)
+    {
+        if (category.Name == category.DisplayOrderType.ToString())
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+        
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated succesfuly";
+            return RedirectToAction("Index");    
+        }
+
+        return View(category);
+    }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category category)
+    {
+        if (category.Name == category.DisplayOrderType.ToString())
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+        
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created succesfuly";
+            return RedirectToAction("Index");    
+        }
+
+        return View(category);
+    }
+    
+    
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+            return NotFound();
+
+        var category = _db.Categories.Find(id);
+        // var categoryFromFirst = _db.Categories.FirstOrDefault(category => category.Id == id);
+        // var categoryFromSingle = _db.Categories.SingleOrDefault(category => category.Id == id);
+
+        if (category == null)
+            return NotFound();
+        
+        return View(category);
+    }
+    
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var category = _db.Categories.Find(id);
+        
+        if (category == null)
+            return NotFound();
+
+        _db.Categories.Remove(category);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted succesfuly";
+
+        return RedirectToAction("Index");
+    }
 }
